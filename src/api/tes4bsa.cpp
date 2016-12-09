e771a4f2e78d006e0e68051640b4162c74f9c648
knecht <knecht@snorc.org>
2016-12-09 20:58:34 +0100

    fixed paths of corruption

--- src/api/tes4bsa.cpp
+++ src/api/tes4bsa.cpp
@@ -100,8 +100,9 @@ namespace libbsa {
                     if (!folderName.empty())
                         fileData.path = folderName + '\\';
 
-                    fileData.path += getFileName(fileNames, fileNameListPos);
-                    fileNameListPos += fileData.path.length() + 1;
+                    std::string fn = getFileName(fileNames, fileNameListPos);
+                    fileData.path += fn;
+                    fileNameListPos += fn.length() + 1;
 
                     //Finally, store file data.
                     assets.push_back(fileData);
